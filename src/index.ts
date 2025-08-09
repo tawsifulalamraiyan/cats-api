4732b3e0116f6dcb2ba0d24b8f0b3cc8dd304a46
raiyanplanet <tawsifulalam2002@gmail.com>
2025-08-09 09:22:42 +0600

    first commit

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,8 @@ const PORT = 3000;
 
 app.use(cors()); // allow all origins
 
+app.get("/", () => "Hello from Elysia!");
+
 app.get("/api", async () => {
   const catdata = await Cats_data.find().sort({ createdAt: -1 });
   return catdata;
@@ -30,3 +32,5 @@ app.listen(PORT);
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}: http://localhost:${PORT}`
 );
+
+export default app.handle;
